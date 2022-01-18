6727d50b9c145075ff9cc9a53024b5b72f152827
James Wienecke <james.a.wienecke@gmail.com>
2022-01-18 12:17:11 -0600

    Setting up song class.

---
+++ src/Song.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class Song {
+    private String artist;
+    private String title;
+    private List<String> lyrics;
+
+    public Song(String artist, String title, List<String> lyrics) {
+        this.artist = artist;
+        this.title = title;
+        this.lyrics = lyrics;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getLyrics() {
+        return lyrics;
+    }
+
+    public void setLyrics(List<String> lyrics) {
+        this.lyrics = lyrics;
+    }
+}
